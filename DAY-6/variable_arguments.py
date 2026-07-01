7ca7a2361a7c9d7ab4ac077fffa30d8a1fe960ec
Vanshika4705 <vanshikapabbi@gmail.com>
2026-07-01 22:58:55 +0530

    variable_arguments.py

---
+++ DAY-6/variable_arguments.py
@@ -0,0 +1,9 @@
+def add_numbers(*args):
+    print(args) # Tuple
+    print(type(args))
+    print(sum(args))
+
+
+add_numbers(10, 20, 30, 40, 50)
+add_numbers(10, 20, 30)
+add_numbers(-10, -20, -30)
