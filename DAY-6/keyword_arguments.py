1e92420fc500eef09b2fca077b9d2818e8c02744
Vanshika4705 <vanshikapabbi@gmail.com>
2026-07-01 23:01:50 +0530

    keyword_arguments.py

---
+++ DAY-6/keyword_arguments.py
@@ -0,0 +1,14 @@
+def fun(**kwargs):
+    print(kwargs)
+    print(type(kwargs))
+
+
+fun(a=10, b=20, c=30, d='john')
+fun(name='john', email='john@example.com', phone='+919876543210')
+
+
+def fun(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+fun(10, 'john', 30, a=10, b=20, c=30)
