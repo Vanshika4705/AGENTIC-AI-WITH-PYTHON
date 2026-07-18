43ec365bd01b68ae7fa5eb453a1475136a090dc2
Vanshika4705 <vanshikapabbi@gmail.com>
2026-07-18 23:10:20 +0530

    str_ascii_iterator.py

---
+++ DAY-17/str_ascii_iterator.py
@@ -0,0 +1,8 @@
+orders = 'Pizza'
+orders_iterator = iter(orders)
+print(orders_iterator, type(orders_iterator))
+print(next(orders_iterator))
+print(next(orders_iterator))
+print(next(orders_iterator))
+print(next(orders_iterator))
+print(next(orders_iterator))
