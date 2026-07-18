540f4a72f1ec494f77d5a71ca54e8f1341fe30e2
Vanshika4705 <vanshikapabbi@gmail.com>
2026-07-18 23:01:57 +0530

    List_iterator.py

---
+++ DAY-17/List_iterator.py
@@ -0,0 +1,8 @@
+orders = ['Pizza', 'Burger', 'Pasta', 'Noodles', 'Coke']
+orders_iterator = iter(orders)
+print(orders_iterator, type(orders_iterator))
+print(next(orders_iterator))
+print(next(orders_iterator))
+print(next(orders_iterator))
+print(next(orders_iterator))
+print(next(orders_iterator))
