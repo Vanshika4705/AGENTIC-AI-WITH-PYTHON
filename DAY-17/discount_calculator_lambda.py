8208fad30cdf414e57ca27a44e3267cc43530568
Vanshika4705 <vanshikapabbi@gmail.com>
2026-07-18 23:33:16 +0530

    discount_calculator_lambda.py

---
+++ DAY-17/discount_calculator_lambda.py
@@ -0,0 +1,7 @@
+discount_generator=lambda discount : (lambda price: price - (price*discount/100))
+monthly_subcription = discount_generator(10)
+half_yearly_subscription = discount_generator(20)
+yearly_subcription = discount_generator(30)
+print(monthly_subcription(1000))
+print(half_yearly_subscription(1000))
+print(yearly_subcription(1000))
