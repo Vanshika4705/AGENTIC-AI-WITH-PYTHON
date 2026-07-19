d4abb656a51e9ea0037c33a4774e1cc9355394fc
Vanshika <vanshikapabbi@gmail.com>
2026-07-19 22:40:42 +0530

    tuple_indexing.py

---
+++ DAY-18/tuple_indexing.py
@@ -0,0 +1,6 @@
+numbers = (10, 20, 30, 40, 50)
+print("Tuple:", numbers)
+print("First element:", numbers[0])
+print("Second element:", numbers[1])
+print("Last element:", numbers[-1])
+print("Second last element:", numbers[-2])
