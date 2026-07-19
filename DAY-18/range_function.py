5db97f010c968cd2856ffa91531947d211ad6864
Vanshika <vanshikapabbi@gmail.com>
2026-07-19 22:49:46 +0530

    range_function.py

---
+++ DAY-18/range_function.py
@@ -0,0 +1,15 @@
+print("range(5):")
+for i in range(5):
+    print(i)
+
+print()
+
+print("range(1, 6):")
+for i in range(1, 6):
+    print(i)
+
+print()
+
+print("range(0, 11, 2):")
+for i in range(0, 11, 2):
+    print(i)
