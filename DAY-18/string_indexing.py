36366f2a214ddb23581be57065d38c21a20c2cb2
Vanshika <vanshikapabbi@gmail.com>
2026-07-19 22:41:58 +0530

    string_indexing.py

---
+++ DAY-18/string_indexing.py
@@ -0,0 +1,6 @@
+text = "Python"
+print("String:", text)
+print("First character:", text[0])
+print("Second character:", text[1])
+print("Last character:", text[-1])
+print("Second last character:", text[-2])
