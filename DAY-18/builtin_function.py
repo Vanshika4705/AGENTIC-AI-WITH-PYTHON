01afc74001a83e7a02a0d5913233527ccf17c293
Vanshika <vanshikapabbi@gmail.com>
2026-07-19 22:54:59 +0530

    builtin_function.py

---
+++ DAY-18/builtin_function.py
@@ -0,0 +1,11 @@
+numbers = [10, 20, 30, 40, 50]
+
+print("List:", numbers)
+
+print("Sum:", sum(numbers))
+print("Minimum Value:", min(numbers))
+print("Maximum Value:", max(numbers))
+print("Length:", len(numbers))
+
+average = sum(numbers) / len(numbers)
+print("Average:", average)
